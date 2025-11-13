2c517561e1eab67e346133e5aec86af6a24a014c
spud <spud@dadatypo.net>
2025-11-13 18:31:42 -0500

    Added 'sphinx_rtd_theme' as extension, and as theme

--- conf.py
+++ conf.py
@@ -8,15 +8,15 @@ release = '4.0b'
 # Sphinx ≥5 prefers root_doc; master_doc still works on older versions.
 root_doc = 'index'  # was: master_doc = 'index'
 
-extensions = []
+extensions = ['sphinx_rtd_theme']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # On Read the Docs, you can omit this and RTD will use its theme automatically.
 # If you want to pin it explicitly, ensure the package is available on RTD:
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 # Otherwise, remove the next line:
-html_theme = 'default'
+#html_theme = 'default'
 
 html_static_path = ['_static']
 
