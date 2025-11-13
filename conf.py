59a96843ac5f686430170a0d9d009d5cb33faa8c
spud <spud@dadatypo.net>
2025-11-13 17:56:02 -0500

    Updated per ChatGPT. Fingers crossed.

--- conf.py
+++ conf.py
@@ -1,67 +1,29 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 import os
 
-
-# -- Project information -----------------------------------------------------
-
 project = 'Manifesto Documentation'
-copyright = '2025, a.h.s. boy'
 author = 'a.h.s. boy'
-
-# The full version, including alpha/beta/rc tags
+copyright = '2025, a.h.s. boy'
 release = '4.0b'
 
-# The master toctree document.
-master_doc = 'index'
+# Sphinx ≥5 prefers root_doc; master_doc still works on older versions.
+root_doc = 'index'  # was: master_doc = 'index'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
-
-# Add any paths that contain templates here, relative to this directory.
+extensions = []
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# On Read the Docs, you can omit this and RTD will use its theme automatically.
+# If you want to pin it explicitly, ensure the package is available on RTD:
+#   html_theme = 'sphinx_rtd_theme'
+# Otherwise, remove the next line:
+# html_theme = 'default'
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'default'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# Set canonical URL from the Read the Docs Domain
+# Optional; mainly useful with extensions like sphinx-sitemap
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
 
-# Tell Jinja2 templates the build is running on Read the Docs
+# Make sure html_context exists before assigning to it
+html_context = {}
 if os.environ.get("READTHEDOCS", "") == "True":
     html_context["READTHEDOCS"] = True
